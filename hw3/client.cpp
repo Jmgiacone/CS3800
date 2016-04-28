6c2b117605a6ee89c6332408bd5bb73f5ce61137
Jordan Giacone <Jmgiacone@gmail.com>
2016-04-27 19:42:18 -0500

    Preliminary client finished Added in: - Threading - Implemented readFromServer - Implemented writeToServer - pthread_join and pthread_exit

--- hw3/client.cpp
+++ hw3/client.cpp
@@ -4,6 +4,9 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <strings.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <string.h>
 
 using std::cout;
 using std::cin;
@@ -12,13 +15,20 @@ using std::string;
 const int PORT_NUMBER = 9999;
 
 void controlCSignalHandler(int signal);
+void* readFromServer(void* argument);
+void* writeToServer(void* argument);
 
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+const int BUFFER_SIZE = 512;
+int socketNum = -1;
+char buffer[BUFFER_SIZE];
+const string QUIT_COMMAND = "/quit";
+string username = "";
 int main(int argc, char* argv[])
 {
-  string username = "";
-  int socketNum = 0;
   struct sockaddr_in host = {AF_INET, htons(PORT_NUMBER)};
   struct hostent *hostPointer;
+  pthread_t readThread, writeThread;
 
   //A single CLI argument. Hopefully it's the hostname
   if(argc == 2)
@@ -44,6 +54,31 @@ int main(int argc, char* argv[])
         {
           cout << "Connection successful!\nPlease enter a username: ";
           cin >> username;
+          string str = username + "has joined the chat\n";
+          write(socketNum, str.c_str(), BUFFER_SIZE);
+
+          //Try to create the reading thread
+          if(pthread_create(&readThread, NULL, readFromServer, NULL) == 0)
+          {
+            if(pthread_create(&writeThread, NULL, writeToServer, NULL) == 0)
+            {
+              pthread_join(readThread, NULL);
+              pthread_join(writeThread, NULL);
+
+              pthread_exit(&readThread);
+              pthread_exit(&writeThread);
+            }
+            else
+            {
+              cout << "Error: Could not create write thread" << endl;
+              exit(1);
+            }
+          }
+          else
+          {
+            cout << "Error: Could not create read thread" << endl;
+            exit(1);
+          }
         }
         else
         {
@@ -74,4 +109,44 @@ int main(int argc, char* argv[])
 void controlCSignalHandler(int signal)
 {
   cout << "Please don't exit the client with ctl-c. Only use /exit, /part, or /quit";
+}
+
+void* readFromServer(void* argument)
+{
+  string str;
+
+  while(true)
+  {
+    pthread_mutex_lock(&mutex);
+    read(socketNum, buffer, 512);
+
+    str = buffer;
+    pthread_mutex_unlock(&mutex);
+    //Server told client to quit
+    if(str == QUIT_COMMAND)
+    {
+      return NULL;
+    }
+    cout << str << endl;
+  }
+}
+
+void* writeToServer(void* argument)
+{
+  char* x;
+  string sx = username + ": ";
+  char* s;
+  strcpy(s, sx.c_str());
+  while(true)
+  {
+    pthread_mutex_lock(&mutex);
+    x = gets(buffer);
+
+    if(x == NULL)
+    {
+      return NULL;
+    }
+    write(socketNum, strcat(s, buffer), BUFFER_SIZE);
+    pthread_mutex_unlock(&mutex);
+  }
 }
