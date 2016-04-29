c32ed1e21eb8ddd59eb32bc7b7809bf29cd4f8a4
Jordan Giacone <Jmgiacone@gmail.com>
2016-04-28 23:52:24 -0500

    Changed max client size

--- hw3/server.cpp
+++ hw3/server.cpp
@@ -23,7 +23,7 @@
 //server port
 #define SERVER_PORT 9993
 //max number of clients
-#define MAX_CLIENT 100
+#define MAX_CLIENT 10
 
 //handles clients on a per client basis and is responsible for sending messages out to all clients
 //these threads are removed if a client disconnects or the server is shutdown
